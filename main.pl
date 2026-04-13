2da48b1f2a0a4310c859f14091ce9568c41cac96
-inf <infgotoinf@gmail.com>
2026-04-13 17:27:54 +0300

    Rewrite this thing so it also checks the exit code. Mess up with the code a little bit.

--- main.pl
+++ main.pl
@@ -5,36 +5,43 @@ use utf8;
 use Furl;
 
 
-my $link = 'https://raw.githubusercontent.com/SoliSpirit/mtproto/master/all_proxies.txt';
+# my $link = 'https://raw.githubusercontent.com/SoliSpirit/mtproto/master/all_proxies.txt';
+my $link = 'https://cdn.jsdelivr.net/gh/proxifly/free-proxy-list@main/proxies/all/data.txt';
 
 my $wait = 1;
 my $furl = Furl->new(timeout => 5);
 my @proxy_list = split( /\n/, $furl->get($link)->content );
-my @link_list = ('t.me');
+# my @link_list = ('t.me');
+# my @link_list = ('www.reaper.fm');
+my @link_list = ('youtube.com');
 
 my %proxy_hash;
 my $counter = @proxy_list;
 foreach my $proxy (@proxy_list) {
     say $counter--, '..';
-    my $parsed_proxy = $proxy =~
-            s/
-            ^.* server=(?<server> [^&]+) .*$
+    # my $parsed_proxy = $proxy =~
+    #         s/
+    #         ^.* server=(?<server> [^&]+) .*$
 
-            /$+{server}/xr;
+    #         /$+{server}/xr;
+
+    my $parsed_proxy = $proxy;
 
     foreach my $link (@link_list) {
         # say $parsed_proxy;
         open my $fh, '-|',
-        "curl -s -x GET -o /dev/null --write-out '\%{time_total}' --proxy $parsed_proxy -m $wait $link";
+        "curl -s -x GET -o /dev/null --write-out '\%{exitcode} \%{time_total}' --proxy $parsed_proxy -m $wait $link";
+
         while (my $line = <$fh>) {
-            $proxy_hash{$proxy} .= $line;
+            if ($line =~ /^(?<exit_code>\d+) (?<time>.+)/) {
+                if ($+{exit_code} == 0 && $+{time} < $wait) {
+                    say "$line - $proxy";
+                    $proxy_hash{$proxy} .= $line;
+                }
+            }
         }
     }
 }
-
-# sub by_time {
-#     if %_ >
-# }
 
 foreach (sort {$proxy_hash{$a} <=> $proxy_hash{$b}} keys %proxy_hash) {
     say "$proxy_hash{$_} - $_" if $proxy_hash{$_} < $wait;
